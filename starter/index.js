a2fa883b393fc66b0df74b3b97aba44396596733
lauralp92 <115834312+lauralp92@users.noreply.github.com>
2023-01-29 22:20:41 +0000

    added question data to index.js

--- starter/index.js
+++ starter/index.js
@@ -10,6 +10,91 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const managerData = [
+  {
+    type: "input",
+    message: "Please enter the team Manager's name:",
+    name: "managerName",
+  },
+
+  {
+    type: "input",
+    message: "Please enter the team Manager's ID:",
+    name: "managerID",
+  },
+
+  {
+    type: "input",
+    message: "Please enter the team Manager's email address:",
+    name: "managerEmail",
+  },
+
+  {
+    type: "input",
+    message: "Please enter the team Manager's office number:",
+    name: "officeNum",
+  },
+];
+
+const employeeMenu = [
+  {
+    type: "list",
+    message: "Please select either an Engineer or Intern to add to the team:",
+    choices: ["Engineer", "Intern"],
+    name: "empType",
+  },
+];
+
+const engData = [
+  {
+    type: "input",
+    message: "Please enter the engineer's name:",
+    name: "engName",
+  },
+
+  {
+    type: "input",
+    message: "Please enter the engineer's ID:",
+    name: "engID",
+  },
+
+  {
+    type: "input",
+    message: "Please enter the engineer's email address:",
+    name: "engEmail",
+  },
+
+  {
+    type: "input",
+    message: "Please enter the engineer's GitHub username",
+    name: "engGithub",
+  },
+];
+
+const internData = [
+  {
+    type: "input",
+    message: "Please enter the intern's name:",
+    name: "intName",
+  },
+
+  {
+    type: "input",
+    message: "Please enter the intern's ID:",
+    name: "intID",
+  },
+
+  {
+    type: "input",
+    message: "Please enter the intern's email address:",
+    name: "intEmail",
+  },
+
+  {
+    type: "input",
+    message: "Please enter the name of the intern's university/school",
+    name: "intSch",
+  },
+];
