28aeed29e635d8addb7f698dabda11d862652f6c
lauralp92 <115834312+lauralp92@users.noreply.github.com>
2023-01-30 20:05:16 +0000

    corrected const error

--- starter/lib/Intern.js
+++ starter/lib/Intern.js
@@ -1,6 +1,6 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
 
-const Intern = require("./lib/Employee");
+const Employee = require("./lib/Employee");
 
 class Intern extends Employee {
   constructor(name, id, email, school) {
