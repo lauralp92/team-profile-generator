9399221aaaa7a1dbb8c85c182d840ea923e324d8
lauralp92 <115834312+lauralp92@users.noreply.github.com>
2023-02-05 16:26:10 +0000

    added questions to functions

--- starter/index.js
+++ starter/index.js
@@ -10,91 +10,95 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const teamArr = [];
+
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-const managerData = [
-  {
-    type: "input",
-    message: "Please enter the team Manager's name:",
-    name: "managerName",
-  },
+// Questions for the user regarding team manager
+function addManagerData() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Please enter the team manager's name:",
+      name: "managerName",
+    },
+    {
+      type: "input",
+      message: "Please enter the team manager's ID:",
+      name: "managerID",
+    },
+    {
+      type: "input",
+      message: "Please enter the team manager's email address:",
+      name: "managerEmail",
+    },
+    {
+      type: "input",
+      message: "Please enter the team manager's office number:",
+      name: "officeNum",
+    },
+  ]);
+}
 
-  {
-    type: "input",
-    message: "Please enter the team Manager's ID:",
-    name: "managerID",
-  },
+function employeeMenu() {
+  inquirer.prompt([
+    {
+      type: "list",
+      message: "Please select either an Engineer or Intern to add to the team",
+      choices: ["Engineer", "Intern"],
+      name: "empType",
+    },
+  ]);
+}
 
-  {
-    type: "input",
-    message: "Please enter the team Manager's email address:",
-    name: "managerEmail",
-  },
+function addEngData() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Please enter the engineer's name:",
+      name: "engName",
+    },
+    {
+      type: "input",
+      message: "Please enter the engineer's ID:",
+      name: "engID",
+    },
+    {
+      type: "input",
+      message: "Please enter the engineer's email address:",
+      name: "engEmail",
+    },
 
-  {
-    type: "input",
-    message: "Please enter the team Manager's office number:",
-    name: "officeNum",
-  },
-];
+    {
+      type: "input",
+      message: "Please enter the engineer's GitHub username",
+      name: "engGitHub",
+    },
+  ]);
+}
 
-const employeeMenu = [
-  {
-    type: "list",
-    message: "Please select either an Engineer or Intern to add to the team:",
-    choices: ["Engineer", "Intern"],
-    name: "empType",
-  },
-];
+function addIntData() {
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "Please enter the intern's name:",
+      name: "intName",
+    },
+    {
+      type: "input",
+      message: "Please enter the intern's ID:",
+      name: "intID",
+    },
+    {
+      type: "input",
+      message: "Please enter the intern's email address:",
+      name: "intEmail",
+    },
 
-const engData = [
-  {
-    type: "input",
-    message: "Please enter the engineer's name:",
-    name: "engName",
-  },
-
-  {
-    type: "input",
-    message: "Please enter the engineer's ID:",
-    name: "engID",
-  },
-
-  {
-    type: "input",
-    message: "Please enter the engineer's email address:",
-    name: "engEmail",
-  },
-
-  {
-    type: "input",
-    message: "Please enter the engineer's GitHub username",
-    name: "engGithub",
-  },
-];
-
-const internData = [
-  {
-    type: "input",
-    message: "Please enter the intern's name:",
-    name: "intName",
-  },
-
-  {
-    type: "input",
-    message: "Please enter the intern's ID:",
-    name: "intID",
-  },
-
-  {
-    type: "input",
-    message: "Please enter the intern's email address:",
-    name: "intEmail",
-  },
-
-  {
-    type: "input",
-    message: "Please enter the name of the intern's university/school",
-    name: "intSch",
-  },
-];
+    {
+      type: "input",
+      message: "Please enter the intern's university/school",
+      name: "intSch",
+    },
+  ]);
+}
